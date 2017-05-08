376af50256f307d06315ef3d2550208b47cfd1c6
MontherAhmed <keepmonther95@gmail.com>
2017-05-08 04:34:13 -0700

    Update Login2.aspx.cs

--- Login2.aspx.cs
+++ Login2.aspx.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Data;
 using System.Data.SQLite;
-using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,27 +11,30 @@ using System.Web.UI.WebControls;
     public partial class Login2 : System.Web.UI.Page
     {
 
-    
-
-  
-
-
-    public void Page_Load(object sender, EventArgs e)
-        {
-
-
-        }
 
 
 
-        protected void Login_Click(object sender, EventArgs e)
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+
+
+
+
+    protected void Login_Click(object sender, EventArgs e)
         {
 
       
 
-        String query1 = "SELECT Employee.Emp_ID, Password.Emp_Pw, Employee.Level FROM   Password, Employee GROUP BY Employee.Emp_ID HAVING (Employee.Emp_ID ="+Login_E_ID.Text+") AND (Password.Emp_Pw ="+Login_P.Text+")";
+        String query1 = "SELECT Employee.Emp_name, Employee.Emp_ID, Employee.Level FROM   Password, Employee GROUP BY Employee.Emp_ID HAVING (Employee.Emp_ID =" + Login_E_ID.Text+") AND (Password.Emp_Pw ="+Login_P.Text+")";
 
-        using (SQLiteConnection con = new SQLiteConnection(@"data source = D:\HoSsAm\Gam3a L7\Demo\asp-siminta-admin v5\asp-siminta-admin v5\SAFS.sqlite"))
+        using (SQLiteConnection con = new SQLiteConnection(@"data source = C:\Users\Monther\Desktop\asp-siminta-admin v5\asp-siminta-admin v5\SAFS.sqlite"))
         {
 
 
@@ -50,17 +52,24 @@ using System.Web.UI.WebControls;
 
 
                 String Name = dt.Rows[0][0].ToString();
-                String password = dt.Rows[0][1].ToString();
+                String ID = dt.Rows[0][1].ToString();
                 String Level = dt.Rows[0][2].ToString();
 
-           
 
+
+
+                Session["NAM"] = Name;
+                Session["ID"] = ID;
+                Session["LE"] = Level;
 
 
 
                 Response.Redirect("~/Default.aspx");
 
                 Label1.Visible = false;
+
+
+
 
             }
 
